d29f707aaf4a86f32106222467eb3cc6e668f95e
Andrew Jackson <dorkmeisterx69@gmail.com>
2017-05-02 22:10:40 -0500

    Update for searilzation

--- functions/webhook.py
+++ functions/webhook.py
@@ -8,11 +8,17 @@ import time
 import uuid
 import boto3
 import requests
+import decimal
+
+def default(obj):
+    if isinstance(obj, decimal.Decimal):
+        return int(obj)
+    return o.__dict__
 
 def handler(event, context):
     print "event.dump = " + json.dumps(event)
-    url = event['webhookurl']
-    payload = {json.dumps(event['payload'])}
+    url = 'https://discordapp.com/api/webhooks/308790391031726082/zGRwUsO66V6UXl4Vb8eg8lDZ2pE7eljfPMQLVF9lSJlytAoYOCDSSsi8wUe9F1shC2IJ/github'
+    payload = {json.dumps(event)}
     headers = {'content-type': 'application/json'}
 
-    r = requests.post(url, data=json.dumps(payload), headers=headers)
+    r = requests.post(url, data=json.dumps(payload), headers=headers, default=default)
