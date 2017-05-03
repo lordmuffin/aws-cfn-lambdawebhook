20984a50ae37f5828c15c60ee34255c679ae57d0
Andrew Jackson <dorkmeisterx69@gmail.com>
2017-05-02 21:37:43 -0500

    update zip for lambda function

--- lambdazip.sh
+++ lambdazip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 (echo "### Running Git Clone"; git clone $CIRCLE_REPOSITORY_URL $HOME/$CIRCLE_PROJECT_REPONAME)
 (echo "### Make Zip Folder"; sudo mkdir $HOME/upload; )
-(echo "### Zipping Lambda Files";cd $HOME/$CIRCLE_PROJECT_REPONAME/; sudo zip -r9 $zipfile *)
+(echo "### Zipping Lambda Files";cd $HOME/$CIRCLE_PROJECT_REPONAME/functions/; sudo zip -r9 $zipfile *)
 (echo "### Zipping Site-Packages"; cd $VIRTUAL_ENV/lib/python2.7/site-packages; sudo zip -r9 $zipfile *)
 echo "### Good or bad im done. $zipfile Created."
