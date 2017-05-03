e57e9fdf6af58f13218b923578c1f2181c6e4b0a
Andrew Jackson <dorkmeisterx69@gmail.com>
2017-05-02 19:09:30 -0500

    Add example json as well as generalize webhook function.

--- functions/webhook.py
+++ functions/webhook.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # Written by: Andrew Jackson
-# This is used to pull repo from github and drop to S3
+# This is used to send a JSON payload to a webhook.
 import json
 import logging
 import os
@@ -10,8 +10,8 @@ import boto3
 
 def handler(event, context):
     print "event.dump = " + json.dumps(event)
-    url = 'https://discordapp.com/api/webhooks/308790391031726082/zGRwUsO66V6UXl4Vb8eg8lDZ2pE7eljfPMQLVF9lSJlytAoYOCDSSsi8wUe9F1shC2IJ/github'
-    payload = {json.dumps(event)}
+    url = event['webhookurl']
+    payload = {json.dumps(event['payload'])}
     headers = {'content-type': 'application/json'}
 
     r = requests.post(url, data=json.dumps(payload), headers=headers)
